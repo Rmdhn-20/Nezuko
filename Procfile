6489f4a7d4d788b8bd215df41137148c8c4cee77
Rmdhn-20 <ramadhanart3@gmail.com>
2022-03-09 13:44:05 +0700

    First Commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+worker: npm i && npm i yt-search@latest && npm start
---
+++ config.js
@@ -0,0 +1,49 @@
+/**
+   * Create By Dika Ardnt.
+   * Contact Me on wa.me/6288292024190
+   * Follow https://github.com/DikaArdnt
+*/
+
+const fs = require('fs')
+const chalk = require('chalk')
+
+// Website Api
+global.APIs = {
+	zenz: 'https://zenzapi.xyz',
+	dapuhy: 'https://dapuhy.xyz',
+        leys: 'http://leyscoders-api.herokuapp.com',
+}
+
+// Apikey Website Api
+global.APIKeys = {
+	'https://zenzapi.xyz': 'exz123',
+	'https://dapuhy.xyz': 'ekaa123',
+        'http://leyscoders-api.herokuapp.com': 'dappakntlll',
+}
+
+// Other
+global.owner = ['6289618777587','6289618777587','6289618777587','6289618777587','6289618777587']
+global.packname = '\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\nēkนຊikaོ'
+global.author = '\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\nNezuko-Bot'
+global.sessionName = 'hisoka'
+global.prefa = ['','!','.','🐦','🐤','🗿']
+global.sp = '⭔'
+global.mess = {
+    success: '[ ✓ ] Success',
+    admin: '[ ! ] Fitur Khusus Admin Group',
+    botAdmin: 'Bot Harus Menjadi Admin Terlebih Dahulu!',
+    owner: '[ ! ] Fitur Khusus Owner Bot',
+    group: 'Fitur Digunakan Hanya Untuk Group!',
+    private: 'Fitur Digunakan Hanya Untuk Private Chat!',
+    bot: '[ ! ] Fitur Khusus Pengguna Nomor Bot',
+    wait: '[ ⏳ ] Loading...',
+}
+global.thumb = fs.readFileSync('./lib/hisoka.jpg')
+
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+	fs.unwatchFile(file)
+	console.log(chalk.redBright(`Update'${__filename}'`))
+	delete require.cache[file]
+	require(file)
+})
